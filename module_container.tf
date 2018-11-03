e59eddaf674735cbc3bd07c57cfee7d66f093bd9
Mark0930 <Mark814690@gmail.com>
2018-11-03 17:56:50 +0000

    added dynaomo codebuild

--- module_container.tf
+++ module_container.tf
@@ -7,3 +7,8 @@ module "api_codebuild" {
   source  = "codebuild"
   project_name= "${var.project_name}-api-codebuild"
 }
+
+module "dynamo_codebuild" {
+  source  = "codebuild"
+  project_name= "${var.project_name}-dynamo-codebuild"
+}
